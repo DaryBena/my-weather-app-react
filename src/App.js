7b3480091946c5ce038f2555b32b26bd0a5ee399
DaryBena <darinabezyakina@gmail.com>
2023-03-07 22:24:53 +0200

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Dary Bena is{" "}
+        <a
+          href="https://github.com/DaryBena/my-weather-app-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
